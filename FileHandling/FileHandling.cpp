ea0a95ea728dc6903953aa6a13319fe2e6ae40b0
Nahid Hasan <nahid.thetechnerd@gmail.com>
2022-08-29 21:20:41 +0600

    File Handling

---
+++ FileHandling/FileHandling.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int t,a,b;
+
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
